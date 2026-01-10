03b471d132d9a88fac67cbc69f91fbcd07f380be
RailTrader Dev <dev@railtrader.com>
2026-01-10 06:41:05 -0800

    Fix localhost URLs in AdminChallengeConfig

--- src/pages/admin/AdminChallengeConfig.js
+++ src/pages/admin/AdminChallengeConfig.js
@@ -3,6 +3,7 @@ import { Plus, Edit, Trash2 } from 'lucide-react';
 import { GlassCard } from '../../components/UIComponents';
 import { useTheme } from '../../contexts/ThemeContext';
 import { useAuth } from '../../contexts/AuthContext';
+import { apiUrl } from '../../utils/api';
 
 const AdminChallengeConfig = () => {
   const { isDark } = useTheme();
@@ -33,7 +34,7 @@ const AdminChallengeConfig = () => {
 
   const fetchPlans = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/admin/challenge-plans', {
+      const response = await fetch(`${apiUrl()}/api/admin/challenge-plans`, {
         headers: { 'Authorization': `Bearer ${token}` }
       });
       const data = await response.json();
@@ -48,8 +49,8 @@ const AdminChallengeConfig = () => {
   const handleSave = async () => {
     try {
       const url = editingPlan 
-        ? `http://localhost:5000/api/admin/challenge-plans/${editingPlan._id}`
-        : 'http://localhost:5000/api/admin/challenge-plans';
+        ? `${apiUrl()}/api/admin/challenge-plans/${editingPlan._id}`
+        : `${apiUrl()}/api/admin/challenge-plans`;
       
       const response = await fetch(url, {
         method: editingPlan ? 'PUT' : 'POST',
@@ -93,7 +94,7 @@ const AdminChallengeConfig = () => {
 
   const handleDelete = async (planId) => {
     try {
-      const response = await fetch(`http://localhost:5000/api/admin/challenge-plans/${planId}`, {
+      const response = await fetch(`${apiUrl()}/api/admin/challenge-plans/${planId}`, {
         method: 'DELETE',
         headers: { 'Authorization': `Bearer ${token}` }
       });
